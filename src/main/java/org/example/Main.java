9cb35effb6ef7ba4fb80d7b0785bfcf7ad05cf7c
misikovich <misikovich.dev@gmail.com>
2023-03-16 19:11:29 +0100

    sdfsdf3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 }
